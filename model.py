127674371d9126192ce5c84ab043edb8f613c4d2
Gregoire Devauchelle <gregoire.devauchelle@gmail.com>
2025-04-07 22:39:19 +0200

    First commit with model and flow to read feed

---
+++ model.py
@@ -0,0 +1,35 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, Text, TIMESTAMP
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import JSON
+
+class Base(DeclarativeBase):
+    pass
+
+class Feed(Base):
+    __tablename__ = "feed"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(Text, nullable=False, unique=True)
+    domain_name = Column(Text, nullable=False)
+    name = Column(Text, nullable=False)
+    last_crawled_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    _created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+
+
+class FeedEntry(Base):
+    __tablename__ = "feed_entry"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    feed_id: Mapped[int] = Column(Integer, ForeignKey("feed.id"))
+    feed_entry_id = Column(Text, unique=True, nullable=False)
+    title = Column(Text, nullable=False)
+    author = Column(Text)
+    publication_date = Column(TIMESTAMP(timezone=True))
+    raw = Column(JSON, nullable=False)
+    _created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
---
+++ read_feed.py
@@ -0,0 +1,76 @@
+from prefect import flow, task, get_run_logger
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, select
+from model import Feed, FeedEntry
+import feedparser
+from datetime import datetime
+import time
+from prefect.blocks.system import Secret
+
+secret_block = Secret.load("pg")
+
+
+# Database config
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://chocolib:{secret_block.get()}@localhost/chocolib"
+)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True)
+
+
+@task
+def get_all_feeds() -> list[Feed]:
+    logger = get_run_logger()
+    logger.info("Fetching all feeds from database...")
+    with Session(engine) as session:
+        feeds = session.scalars(select(Feed)).all()
+        logger.info(f"Retrieved {len(feeds)} feeds.")
+        return feeds
+
+
+@task
+def parse_feed(feed: Feed) -> list[dict]:
+    logger = get_run_logger()
+    logger.info(f"Parsing feed from URL: {feed.url}")
+    parsed = feedparser.parse(feed.url)
+    entries = [
+        entry
+        for entry in parsed.entries
+        if hasattr(entry, "title") and hasattr(entry, "guid")
+    ]
+    logger.info(f"Found {len(entries)} entries in feed: {feed.url}")
+    return entries
+
+
+@task
+def store_feed_entries(feed_id: int, entries: list[dict]):
+    logger = get_run_logger()
+    logger.info(f"Storing {len(entries)} entries for feed ID {feed_id}...")
+    with Session(engine) as session:
+        for entry in entries:
+            feed_entry = FeedEntry(
+                feed_id=feed_id,
+                feed_entry_id=entry.guid,
+                title=entry.title,
+                publication_date=datetime.fromtimestamp(
+                    time.mktime(entry.get("published_parsed"))
+                ),
+                raw=entry,
+            )
+            session.add(feed_entry)
+        session.commit()
+    logger.info(f"Stored entries for feed ID {feed_id}")
+
+
+@flow
+def rss_ingestion_flow():
+    logger = get_run_logger()
+    logger.info("Starting RSS ingestion flow.")
+    feeds = get_all_feeds()
+    for feed in feeds:
+        entries = parse_feed(feed)
+        store_feed_entries(feed.id, entries)
+    logger.info("RSS ingestion flow completed.")
+
+
+if __name__ == "__main__":
+    rss_ingestion_flow()
