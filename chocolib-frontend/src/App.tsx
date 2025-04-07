4d4d598f9b235a961b46be8ad6f0bf1ddaa17a50
Gregoire Devauchelle <gregoire.devauchelle@gmail.com>
2025-04-08 00:28:14 +0200

    Adding vite app

---
+++ chocolib-frontend/src/App.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+interface News {
+  title: string
+}
+
+function App() {
+  const [news, setNews] = useState<News[] | null>(null)
+  const [refresh, setRefresh] = useState<number>(0)
+  const [isFetching, setIsFetching] = useState<boolean>(false)
+
+  function fetchNews() {
+    if (isFetching) return
+
+    setIsFetching(true)
+
+    fetch("https://api.chocolib.com/feed-entries", {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // mode: 'no-cors'
+    }).then(resp => {
+      setIsFetching(false)
+      console.log(resp)
+      if (resp.ok) {
+        resp.json().then(data => {
+          setNews(data)
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchNews()
+  }, [refresh])
+
+  console.log(news)
+
+  return (
+    <>
+      {news?.map(ele => <div key={Math.random()}>{ele.title}</div>)}
+      <button onClick={() => setRefresh(Math.random())}>Refresh</button>
+    </>
+  )
+}
+
+export default App
